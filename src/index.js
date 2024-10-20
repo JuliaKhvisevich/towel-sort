98f254d001e7997fd3f1847acd3a16cb641f9a50
JuliaKhvisevich <reytjulia@gmail.com>
2024-10-20 16:12:20 +0300

    feat: add Reduce method

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+  if (!matrix || matrix.length === 0) {
+    return [];
+  }
+
+  return matrix.reduce((acc, cur, i) => {
+    cur.sort((a, b) => !(i % 2) ? a - b : b - a).forEach(e => acc.push(e));
+    return acc;
+  }, []);
+};
